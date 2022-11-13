ab6273694ac23b53133bf3a12a71b2f017686e2b
rverat <dllw@tutanota.com>
2022-11-13 04:05:49 -0500

    git add home app files .py

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post, Category, Comment, Profile, PostView, Like
+
+#admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Profile)
+admin.site.register(PostView)
+admin.site.register(Like)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publish_date', 'status','author')
+
+
+@admin.register(Comment)
+class ComentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'content','timestamp')
